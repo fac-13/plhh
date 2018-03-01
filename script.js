c090fc705e38bf43b319cb7b6ee253f536f3b416
HA <haydnba@gmail.com>
2018-03-01 14:36:50 +0000

    add full js menu close funcitonality and footer js date Relates #23 & #18

--- script.js
+++ script.js
@@ -2,24 +2,20 @@
 
 // JS functionality to close the manu checkbox when user follows internal link:
 
-const closeMenu = function() {
 
 	// access our checkbox
-	let checkBox = document.querySelector('#menu-check');
+	const checkBox = document.querySelector('#menu-check');
 	// access internal links
-	let menuItem = document.querySelectorAll('.header__item');
+	const menuItems = document.querySelectorAll('.header__item');
 	// on internal link click event uncheck checkbox
-	menuItem.addEventListener('click', function() {
-		checkBox.checked = false
-	});
+	menuItems.forEach(menuItem => menuItem.addEventListener('click', function() {
+		checkBox.checked = false;
+	}));
 	
-}
 
-
-const theYear = function() {
 
 	// access our year-holder span
 	let displayYear = document.querySelector('.js-year');
+	let theYear = new Date().getFullYear();
 	// populate the footer with the year
-	displayYear.textContent = `${new Date(Y)}``
-}
+	displayYear.textContent = theYear;
